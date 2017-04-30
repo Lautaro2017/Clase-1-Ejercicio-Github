ca1cd61685f7b9ea2ee23570eb3508e91978a44b
lautaro2017 <lautarogutierrez4@gmail.com>
2017-04-30 19:12:49 -0300

    Se agregan pruebas de la clase Sistema y Pizarron

--- Obligatorio I/Pruebas/PruebasSistema.cs
+++ Obligatorio I/Pruebas/PruebasSistema.cs
@@ -15,7 +15,17 @@ namespace Pruebas
         {
             Usuario u = utilidad.NuevoUsuario();
             nuevoSistema.AgregarUsuario(u);
-            Assert.IsTrue(nuevoSistema.UsuarioYaIngresado(u));
+            Assert.IsTrue(nuevoSistema.Usuarios.contains(u));
+        }
+
+        [TestMethod]
+        public void AgregarUsuarioYaExistenteNotOK()
+        {
+            Usuario u1 = utilidad.NuevoUsuario();
+            Usuario u2 = utilidad.NuevoUsuario();
+            nuevoSistema.AgregarUsuario(u1);
+            nuevoSistema.AgregarUsuario(u2);
+            Assert.IsFalse(nuevoSistema.Usuarios.contains(u2));
         }
 
         [TestMethod]
@@ -23,7 +33,25 @@ namespace Pruebas
         {
             Equipo e = utilidad.NuevoEquipo();
             nuevoSistema.AgregarEquipo(e);
-            Assert.IsTrue(nuevoSistema.EquipoYaIngresado(e));
+            Assert.IsTrue(nuevoSistema.Equipos.contains(e));
+        }
+
+        [TestMethod]
+        public void AgregarEquipoYaExistenteNotOK()
+        {
+            Equipo e1 = utilidad.NuevoEquipo();
+            Equipo e2 = utilidad.NuevoEquipo();
+            nuevoSistema.AgregarEquipo(e1);
+            nuevoSistema.AgregarEquipo(e2);
+            Assert.IsFalse(nuevoSistema.Equipos.contains(e2));
+        }
+
+        [TestMethod]
+        public void AgregarPizarronOK()
+        {
+            Pizarron p = utilidad.NuevoPizarron();
+            nuevoSistema.AgregarPizarron(p);
+            Assert.IsTrue(nuevoSistema.Pizarrones.contains(p));
         }
     }
 }
