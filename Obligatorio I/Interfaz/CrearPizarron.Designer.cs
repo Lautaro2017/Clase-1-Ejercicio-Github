24256c64483192a40af6a9241e83fd64ede47786
manchiny24 <manchiny24@hotmail.com>
2017-04-30 21:01:02 -0300

    Se crea la pantalla Crear Pizarron

---
+++ Obligatorio I/Interfaz/CrearPizarron.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Interfaz
+{
+    partial class CrearPizarron
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCrearEquipo = new System.Windows.Forms.Label();
+            this.txtAncho = new System.Windows.Forms.TextBox();
+            this.lblAncho = new System.Windows.Forms.Label();
+            this.txtAlto = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.cmbEquipo = new System.Windows.Forms.ComboBox();
+            this.lblEquipo = new System.Windows.Forms.Label();
+            this.rtxtDescripcion = new System.Windows.Forms.RichTextBox();
+            this.lblAlto = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCrearEquipo
+            // 
+            this.lblCrearEquipo.AutoSize = true;
+            this.lblCrearEquipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCrearEquipo.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblCrearEquipo.Location = new System.Drawing.Point(213, 38);
+            this.lblCrearEquipo.Name = "lblCrearEquipo";
+            this.lblCrearEquipo.Size = new System.Drawing.Size(152, 25);
+            this.lblCrearEquipo.TabIndex = 63;
+            this.lblCrearEquipo.Text = "Crear Pizarron";
+            this.lblCrearEquipo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtAncho
+            // 
+            this.txtAncho.Location = new System.Drawing.Point(472, 102);
+            this.txtAncho.Name = "txtAncho";
+            this.txtAncho.Size = new System.Drawing.Size(36, 20);
+            this.txtAncho.TabIndex = 87;
+            // 
+            // lblAncho
+            // 
+            this.lblAncho.AutoSize = true;
+            this.lblAncho.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAncho.Location = new System.Drawing.Point(414, 104);
+            this.lblAncho.Name = "lblAncho";
+            this.lblAncho.Size = new System.Drawing.Size(52, 17);
+            this.lblAncho.TabIndex = 86;
+            this.lblAncho.Text = "Ancho:";
+            // 
+            // txtAlto
+            // 
+            this.txtAlto.Location = new System.Drawing.Point(363, 102);
+            this.txtAlto.Name = "txtAlto";
+            this.txtAlto.Size = new System.Drawing.Size(36, 20);
+            this.txtAlto.TabIndex = 85;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(318, 285);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(101, 25);
+            this.btnCancelar.TabIndex = 84;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(165, 285);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(101, 25);
+            this.btnGuardar.TabIndex = 83;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // cmbEquipo
+            // 
+            this.cmbEquipo.FormattingEnabled = true;
+            this.cmbEquipo.Location = new System.Drawing.Point(132, 142);
+            this.cmbEquipo.Name = "cmbEquipo";
+            this.cmbEquipo.Size = new System.Drawing.Size(134, 21);
+            this.cmbEquipo.TabIndex = 82;
+            // 
+            // lblEquipo
+            // 
+            this.lblEquipo.AutoSize = true;
+            this.lblEquipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEquipo.Location = new System.Drawing.Point(63, 145);
+            this.lblEquipo.Name = "lblEquipo";
+            this.lblEquipo.Size = new System.Drawing.Size(56, 17);
+            this.lblEquipo.TabIndex = 81;
+            this.lblEquipo.Text = "Equipo:";
+            // 
+            // rtxtDescripcion
+            // 
+            this.rtxtDescripcion.Location = new System.Drawing.Point(324, 175);
+            this.rtxtDescripcion.Name = "rtxtDescripcion";
+            this.rtxtDescripcion.Size = new System.Drawing.Size(206, 58);
+            this.rtxtDescripcion.TabIndex = 80;
+            this.rtxtDescripcion.Text = "";
+            // 
+            // lblAlto
+            // 
+            this.lblAlto.AutoSize = true;
+            this.lblAlto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlto.Location = new System.Drawing.Point(321, 105);
+            this.lblAlto.Name = "lblAlto";
+            this.lblAlto.Size = new System.Drawing.Size(36, 17);
+            this.lblAlto.TabIndex = 79;
+            this.lblAlto.Text = "Alto:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(132, 106);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(134, 20);
+            this.txtNombre.TabIndex = 76;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(57, 106);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(62, 17);
+            this.lblNombre.TabIndex = 78;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(321, 143);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(86, 17);
+            this.lblDescripcion.TabIndex = 77;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // CrearPizarron
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtAncho);
+            this.Controls.Add(this.lblAncho);
+            this.Controls.Add(this.txtAlto);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.cmbEquipo);
+            this.Controls.Add(this.lblEquipo);
+            this.Controls.Add(this.rtxtDescripcion);
+            this.Controls.Add(this.lblAlto);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblCrearEquipo);
+            this.Name = "CrearPizarron";
+            this.Size = new System.Drawing.Size(582, 347);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblCrearEquipo;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.RichTextBox rtxtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblAlto;
+        private System.Windows.Forms.Label lblEquipo;
+        private System.Windows.Forms.ComboBox cmbEquipo;
+        private System.Windows.Forms.TextBox txtAlto;
+        private System.Windows.Forms.Label lblAncho;
+        private System.Windows.Forms.TextBox txtAncho;
+    }
+}
