c33bcb88745484822fd6c42d7c6e1b1e029a659f
manchiny24 <manchiny24@hotmail.com>
2017-05-01 19:42:45 -0300

    Se agrega funcionalidad de boton cancelar en Crear Pizarron, Crear Equipo y Crear Usuario

--- Obligatorio I/Interfaz/CrearPizarron.Designer.cs
+++ Obligatorio I/Interfaz/CrearPizarron.Designer.cs
@@ -87,6 +87,7 @@
             this.btnCancelar.TabIndex = 84;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnGuardar
             // 
--- Obligatorio I/Interfaz/CrearPizarron.cs
+++ Obligatorio I/Interfaz/CrearPizarron.cs
@@ -26,5 +26,10 @@ namespace Interfaz
         {
 
         }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
     }
 }
