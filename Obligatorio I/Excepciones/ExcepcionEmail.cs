1666f78215746261151c672c9c6cf6b621e76312
lautaro2017 <lautarogutierrez4@gmail.com>
2017-05-01 17:51:16 -0300

    Se crean excepciones de formato de email y datos vacíos

---
+++ Obligatorio I/Excepciones/ExcepcionEmail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class ExcepcionEmail : Exception
+    {
+        public ExcepcionEmail() : base("El formato de e-mail no es valido") { }
+    }
+}
