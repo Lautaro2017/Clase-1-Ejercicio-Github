6ad10da3f92de6fa81a88df5a6f8c99a52c561bf
lautaro2017 <lautarogutierrez4@gmail.com>
2017-05-07 19:32:26 -0300

    Se agrega controlador para Usuario ya ingresado

---
+++ Obligatorio I/Lógica/ControladorUsuarioYaIngresado.cs
@@ -0,0 +1,23 @@
+﻿using Excepciones;
+using Obligatorio_I;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lógica
+{
+    public class ControladorUsuarioYaIngresado
+    {
+        Sistema s = Sistema.GetInstance();
+
+        public void UsuarioYaIngresado(string email)
+        {
+            if (s.BuscarUsuarioPorMail(email) != null)
+            {
+                throw new ExcepcionUsuarioYaIngresado();
+            }
+        }
+    }
+}
