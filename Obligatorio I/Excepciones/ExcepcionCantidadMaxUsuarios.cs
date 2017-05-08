8525ccca7f2df0a9a4af9aa154f6a7008deadb03
lautaro2017 <lautarogutierrez4@gmail.com>
2017-05-07 23:55:55 -0300

    Se agrego Excepcion para cantidad maxima de usuarios

---
+++ Obligatorio I/Excepciones/ExcepcionCantidadMaxUsuarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class ExcepcionCantidadMaxUsuarios:Exception
+    {
+        public ExcepcionCantidadMaxUsuarios() : base("Ya se ha ingresado la cantidad maxima de usuarios en el equipo!") { }
+    }
+}
