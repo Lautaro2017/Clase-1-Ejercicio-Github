17e1f2a7ccba238a6868a54da7944fd509283607
lautaro2017 <lautarogutierrez4@gmail.com>
2017-05-15 00:37:46 -0300

    Se agregan nuevas excepciones

---
+++ Obligatorio I/Excepciones/ExcepcionElementoSinComentarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class ExcepcionElementoSinComentarios:Exception
+    {
+        public ExcepcionElementoSinComentarios():base("El elemento no contiene comentarios") { }
+    }
+}
